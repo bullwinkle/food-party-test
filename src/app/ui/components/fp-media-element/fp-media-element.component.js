6751acd2a613206d5b198d728742c9c37d5cbf9b
Dmitriy Galnykin <developer085@gmail.com>
2018-05-30 04:22:38 +0700

    media-element component

--- src/app/ui/components/fp-media-element/fp-media-element.component.js
+++ src/app/ui/components/fp-media-element/fp-media-element.component.js
@@ -1,14 +1,27 @@
-import styles from './fp-media-element.component.scss';
+import './fp-media-element.component.scss';
 import template from './fp-media-element.html';
 
 export const FpMediaElementComponent = {
   template,
-  styles,
   bindings: {},
   controller: class FpMediaElementComponentController {
-    constructor () {
+    constructor ($interval) {
       "ngInject";
+      this.$interval = $interval;
       console.log(this);
+    }
+
+    $onInit () {
+      this.updateImage();
+    }
+
+    onImageClick (e) {
+      console.log(e);
+      this.updateImage();
+    }
+
+    updateImage () {
+      this.imageSrc = `https://picsum.photos/300/200/?random&${Date.now()}`
     }
   }
 };
